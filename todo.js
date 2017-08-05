14e1f61d9aad84ba90be233d6c7a8eb0e792114c
Walker Cole <besidethebrave@gmail.com>
2017-08-05 12:00:44 -0400

    Added basic functionality

--- todo.js
+++ todo.js
@@ -1,3 +1,16 @@
-function TodoCtrl($scope) {
-    
-}
+angular.module('Todo',[]).
+controller('todoController', ['$scope', function($scope) {
+  $scope.todos = [
+      {'title':'Build ToDo App','done':false}
+  ];
+
+  $scope.addTodo = function(){
+      $scope.todos.push({'title':$scope.newTodo,'done':false})
+      $scope.newTodo = ''
+  }
+  $scope.clearCompleted = function(){
+      $scope.todos = $scope.todos.filter(function(item){
+          return !item.done
+      })
+  }
+}]);
